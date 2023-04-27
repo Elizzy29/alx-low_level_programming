6cab22b34ab8733c07b335b6fa65405470979b0f
Elizzy29 <elizzyugbong@gmail.com>
2023-04-27 13:41:48 -0700

    mytasks

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char);
-int _islower(int c);
-void print_alphabet_x10(void);
-int _abs(int);
-void jack_bauer(void);
-int add(int, int);
-void jack_bauer(void);
-void print_to_98(int n);
-void times_table(void);
-int _isalpha(int c);
-int print_sign(int n);
-int print_last_digit(int);
-void print_times_table(int n);;
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
 
-#endif
+void print_alphabet(void)
+{
+	char c = 'a';
 
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-Write a program that prints _putchar, followed by a new line
+this is my project
